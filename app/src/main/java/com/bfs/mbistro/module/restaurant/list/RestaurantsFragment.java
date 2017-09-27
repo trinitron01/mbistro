cb6ad0ab83aecd1fe772cd32e21ff690945eacff
Bartłomiej Sory <bsory@pl.sii.eu>
2017-09-27 11:53:40 +0200

    Gradle updates

--- app/src/main/java/com/bfs/mbistro/module/restaurant/list/RestaurantsFragment.java
+++ app/src/main/java/com/bfs/mbistro/module/restaurant/list/RestaurantsFragment.java
@@ -3,7 +3,6 @@ package com.bfs.mbistro.module.restaurant.list;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.DividerItemDecoration;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -49,8 +48,6 @@ public class RestaurantsFragment extends
     LayoutAnimationController animation = AnimationUtils.loadLayoutAnimation(getContext(), resId);
     recyclerView.setLayoutAnimation(animation);
     progressBar = view.findViewById(R.id.loadingView);
-    recyclerView.setLayoutManager(
-        new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
     recyclerView.setHasFixedSize(true);
     recyclerView.addItemDecoration(
         new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
